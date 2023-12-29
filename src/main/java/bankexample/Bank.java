52a6a5e8ec4a8f16e696f61e9e6ff8473a83fa19
Dariusz Szyperek <dariusz_szyperek@epam.com>
2023-12-29 12:06:46 +0100

    Added other variations of transfer method, and counter for threads

--- src/main/java/bankexample/Bank.java
+++ src/main/java/bankexample/Bank.java
@@ -1,13 +1,17 @@
 package bankexample;
 
-import java.util.Random;
+import java.util.*;
+import java.util.concurrent.locks.ReentrantLock;
 import java.util.logging.Logger;
 
 public class Bank {
 
     private static final Logger LOGGER = Logger.getLogger(Bank.class.getName());
+    private final ReentrantLock lockWithFairAccess = new ReentrantLock(true);
+    private final ReentrantLock lockWithDefaultAccess = new ReentrantLock();
     private final double initialTotalBalance;
     private final double[] accounts;
+    private final Map<Thread, Integer> threadTransferCount = new HashMap<>();
 
     public Bank(int numAccounts) {
         accounts = new double[numAccounts];
@@ -24,13 +28,50 @@ public class Bank {
         }
     }
 
-    public synchronized void transfer(int from, int to, double amount) {
+    public void transfer(int from, int to, double amount) {
+        lockWithFairAccess.lock();
+
+        try {
+            System.out.print(Thread.currentThread());
+            accounts[from] -= amount;
+            System.out.printf(" %10.2f from %d to %d", amount, from, to);
+            accounts[to] += amount;
+            System.out.printf(" Total Balance initially was: %10.2f, and now is: %10.2f%n", initialTotalBalance, getTotalBalance());
+            threadTransferCount.put(Thread.currentThread(), threadTransferCount.getOrDefault(Thread.currentThread(), 0) + 1);
+        } finally {
+            lockWithFairAccess.unlock();
+        }
+    }
+
+    // Default lock which is a non-fair lock will make some threads starve.
+/*
+    public void transfer(int from, int to, double amount) {
+        lockWithDefaultAccess.lock();
+
+        try {
+            System.out.print(Thread.currentThread());
+            accounts[from] -= amount;
+            System.out.printf(" %10.2f from %d to %d", amount, from, to);
+            accounts[to] += amount;
+            System.out.printf(" Total Balance initially was: %10.2f, and now is: %10.2f%n", initialTotalBalance, getTotalBalance());
+            threadTransferCount.put(Thread.currentThread(), threadTransferCount.getOrDefault(Thread.currentThread(), 0) + 1);
+        } finally {
+            lockWithDefaultAccess.unlock();
+        }
+    }
+ */
+
+    // Not synchronized transfers will possibly change total balance, because of Data Race.
+/*
+    public void transfer(int from, int to, double amount) {
         System.out.print(Thread.currentThread());
         accounts[from] -= amount;
         System.out.printf(" %10.2f from %d to %d", amount, from, to);
         accounts[to] += amount;
         System.out.printf(" Total Balance initially was: %10.2f, and now is: %10.2f%n", initialTotalBalance, getTotalBalance());
+        threadTransferCount.put(Thread.currentThread(), threadTransferCount.getOrDefault(Thread.currentThread(), 0) + 1);
     }
+ */
 
     public double getTotalBalance() {
         double total = 0;
@@ -42,5 +83,14 @@ public class Bank {
 
     public int size() {
         return accounts.length;
+    }
+
+    public void printTransferCount() {
+        List<Map.Entry<Thread, Integer>> list = new ArrayList<>(threadTransferCount.entrySet());
+        list.sort(Comparator.comparingLong(o -> o.getKey().getId()));
+
+        for (Map.Entry<Thread, Integer> entry : list) {
+            System.out.println(entry.getKey() + " performed " + entry.getValue() + " transfers.");
+        }
     }
 }
--- src/main/java/bankexample/Demo.java
+++ src/main/java/bankexample/Demo.java
@@ -9,24 +9,56 @@ public class Demo {
 
         Random random = new Random();
 
+//        Runnable r = () -> {
+//            try {
+//                while (!Thread.currentThread().isInterrupted()) {
+//                    int fromAccount = random.nextInt(bank.size());
+//                    int toAccount = random.nextInt(bank.size());
+//                    double amount = (random.nextInt(5000, 10001)) / 100.0; // From 50$ to 100$
+//                    bank.transfer(fromAccount, toAccount, amount);
+//                    Thread.sleep(random.nextInt(5, 50));
+//                }
+//            } catch (InterruptedException e) {
+//                Thread.currentThread().interrupt();
+//            }
+//        };
+
         Runnable r = () -> {
-            try {
-                while (true) {
-                    int fromAccount = random.nextInt(bank.size());
-                    int toAccount = random.nextInt(bank.size());
-                    double amount = (random.nextInt(5000, 10001)) / 100.0; // From 50$ to 100$
-                    bank.transfer(fromAccount, toAccount, amount);
-                    Thread.sleep(random.nextInt(5, 50));
-                }
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            while (!Thread.currentThread().isInterrupted()) {
+                int fromAccount = random.nextInt(bank.size());
+                int toAccount = random.nextInt(bank.size());
+                double amount = random.nextInt(5000, 10001) / 100.0; // From 50$ to 100$
+                bank.transfer(fromAccount, toAccount, amount);
             }
         };
 
+        Thread[] threads = new Thread[100];
+
         // Create and start threads
-        for (int i = 0; i < 1000; i++) {
-            Thread thread = new Thread(r);
-            thread.start();
+        for (int i = 0; i < 100; i++) {
+            threads[i] = new Thread(r);
+            threads[i].start();
         }
+
+        try {
+            Thread.sleep(10000);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
+
+        for (Thread thread : threads) {
+            thread.interrupt();
+        }
+
+        // wait for threads to finish
+        for (Thread thread : threads) {
+            try {
+                thread.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+
+        bank.printTransferCount();
     }
 }
